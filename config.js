40f4033b0bd26f9906485d723abe8ae60eb28ec4
i-ledger <newledger2024@gmail.com>
2025-07-05 15:43:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 
-const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbwUGRL302V__L8zLruRFTPHgKXpP66LVJhpNwUOlymGJrPTEw8WbrqADqzpZSjoOXOq/exec";
+const API_ENDPOINT = "https://script.google.com/macros/s/AKfycbxe7sNGnUwvWc1KhapjWxnIgKpReEATWSgbnloMyv0_Nxj8_8-zM5P1WC95ToCRknoT/exec";
 
 function getAPIUrl(action) {
   const id = localStorage.getItem("id_spreadsheet");
